f59a73a9d26c50268e91b5ebe70fc7d8ce713eff
Juan <jsanchez.gr@gmail.com>
2018-02-01 17:28:26 +0100

    e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,22 @@
 import { AppPage } from './app.po';
 
-describe('websocket App', () => {
+describe('Websocket App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('Should display the welcome screen', () => {
+    page.navigateTo('/');
+    expect(page.getStartMessage()).toEqual('WebSocket');
+  });
+
+  it('Should display the chat', () => {
+    page.navigateTo('/');
+    page.fillInput('author', 'Juan');
+    page.clickButton('Start');
+    page.waitForAngular();
+    expect(page.getWelcomeMessage()).toEqual('Welcome Juan!');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,27 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(url: string) {
+    return browser.get(url);
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getStartMessage() {
+    return element(by.css('h1')).getText();
+  }
+
+  fillInput(id: string, value: string) {
+    element(by.id(id)).sendKeys(value);
+  }
+
+  getWelcomeMessage() {
+    return element(by.id('welcome-msg')).getText();
+  }
+
+  waitForAngular() {
+    browser.waitForAngular();
+  }
+
+  clickButton(text: string) {
+    return element(by.partialButtonText(text)).click();
   }
 }
