e1266822dde89e6c6aee3fa6b4bb1eebc70c8bda
Juan <jsanchez.gr@gmail.com>
2018-02-01 15:51:54 +0100

    Refactor

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -2,13 +2,9 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/map';
 import { WsService } from './ws.service';
+import { Message } from '../models/message.model';
 
 const CHAT_URL = 'ws://echo.websocket.org/';
-
-export interface Message {
-  author: string;
-  message: string;
-}
 
 @Injectable()
 export class ChatService {
@@ -18,11 +14,7 @@ export class ChatService {
     this.messages = <Subject<Message>>wsService
       .connect(CHAT_URL)
       .map((response: MessageEvent): Message => {
-        const data = JSON.parse(response.data);
-        return {
-          author: data.author,
-          message: data.message
-        };
+        return new Message(JSON.parse(response.data));
       });
   }
 }
